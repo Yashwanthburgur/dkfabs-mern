2e46741c01b2f3fd1cf1f2b0e30754d8b01efcae
Yashwanth M <byg2281999@gmail.com>
2026-09-12 05:21:47 +0530

    ui(projects): uncrop project media and move captions below cards

--- frontend/src/components/ProjectsCarousel.jsx
+++ frontend/src/components/ProjectsCarousel.jsx
@@ -48,10 +48,10 @@ const ProjectsCarousel = () => {
               >
                 <div className="project-image">
                   <img src={project.image} alt={project.title} loading="lazy" />
-                  <div className="project-overlay">
-                    <h3>{project.title}</h3>
-                    <p>{project.desc}</p>
-                  </div>
+                </div>
+                <div className="project-caption">
+                  <h3>{project.title}</h3>
+                  <p>{project.desc}</p>
                 </div>
               </div>
             ))}
